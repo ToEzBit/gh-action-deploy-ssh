a04a93c102c9360fa3ce36e2fa150bbbb463e61e
ToEzBit <jirayut52328172@hotmail.com>
2023-12-13 12:32:32 +0700

    :construction_worker:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.json({ message: "DEPLOY_SUCCESSFULLY !!" });
+  res.json({ message: "DEPLOYED !!" });
 });
 
 app.listen(5173, () => console.log("server is running on port 88888"));
